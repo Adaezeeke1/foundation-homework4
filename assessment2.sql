3bad0565557c9296ba58df2d589b7976f665684c
Adaeze Eke <adaeze_eke@outlook.com>
2023-07-11 20:41:27 +0100

    Mistakenly used last assessment. This is the correct answer

--- assessment2.sql
+++ assessment2.sql
@@ -1,62 +1,75 @@
-DROP DATABASE IF EXISTS foundation_assessment_i;
-CREATE DATABASE foundation_assessment_i;
-USE foundation_assessment_i;
+DROP DATABASE IF EXISTS foundation_assessment_ii;
+CREATE DATABASE foundation_assessment_ii;
+USE foundation_assessment_ii;
 
-CREATE TABLE students(
-Student_ID int,
-Forename varchar(50),
-Surname varchar (50)
+CREATE TABLE movie_info (
+    Movie_ID INT,
+    Movie_Name VARCHAR(255),
+    Movie_Length TIME,
+    Age_Rating VARCHAR(10)
 );
 
-INSERT INTO students(Student_ID, FORENAME, SURNAME)
-VALUES
-   (1, "Alice", "Adams"),
-   (2, "Belen", "Badillo"),
-   (3, "Ciara", "Connelly"),
-   (4, "Delia", "Dodds"),
-   (5, "Everly", "Evans"),
-   (6, "Fabia", "Fahim"),
-   (7, "Grace", "Gonzalez")
-;
+ INSERT INTO movie_info(movie_ID, movie_name, movie_length, age_rating)
+ VALUES
+ (1, "The Movie", "2:19:00", "12A"),
+ (2, "The Other Movie", "1:30:00", "15"),
+ (3, "The 3D Amazing Movie",  "1:42:00", "PG"),
+ (4, "La Allure", "1:09:00", "18"),
+ (5, "The Cartoon", "1:15:00", "U"),
+ (6, "The Scary Cartoon", "1:23:00", "PG"),
+ (7, "The Coming Of Age", "1:40:00", "12A"),
+ (8, "The War", "2:07:00", "15"),
+ (9, "The Murder Mystery", "1:47:00", "15");
 
-CREATE TABLE exams(
-Exam_ID int,
-Exam_Name varchar(50),
-Max_Mark int
+CREATE TABLE screens (
+    Screen_ID INT,
+    Four_K BOOLEAN
 );
 
-INSERT INTO exams(Exam_ID, Exam_Name, Max_Mark)
-VALUES
-   (1, "Algorithms", 100),
-   (2, "Cyber Security", 80)
-;
+INSERT INTO screens(screen_ID, four_k)
+ VALUES
+  (1, True),
+  (2, False),
+  (3, True),
+  (4, True),
+  (5, True),
+  (6, True),
+  (7, True),
+  (8, False),
+  (9, True),
+  (10, True);
 
-CREATE TABLE results(
-Result_ID int,
-Student_ID int,
-Exam_ID int,
-Mark int
+CREATE TABLE showings (
+    Showing_ID INT,
+    Movie_ID INT,
+    Screen_ID INT,
+    Start_Time TIME,
+    Available_Seats INT
 );
 
-INSERT INTO results(Result_ID, Student_ID, Exam_ID, Mark)
-VALUES
-   # the results for the first exam
-   (1, 1, 1, 0),
-   (2, 2, 1, 62),
-   (3, 3, 1, 62),
-   (4, 4, 1, 39),
-   (5, 5, 1, 98),
-   (6, 6, 1, 48),
-   (7, 7, 1, 23),
-   # the results for the second exam
-   (8, 1, 2, 43),
-   (9, 2, 2, 68),
-   (10, 3, 2, 54),
-   (11, 4, 2, 21),
-   (12, 5, 2, 68),
-   (13, 6, 2, 74),
-   (14, 7, 2, 14)
-;
+INSERT INTO showings(showing_ID, movie_ID,screen_ID, start_time, available_seats)
+ VALUES
+  (1, 1, 2, '12:00:00', 10),
+  (2, 1, 2, '17:00:00', 23),
+  (3, 2, 9, '10:30:00', 30),
+  (4, 3, 1, '07:00:00', 38),
+  (5, 3, 5, '10:00:00', 26),
+  (6, 3, 1, '17:00:00', 5),
+  (7, 3, 1, '19:00:00', 0),
+  (8, 3, 5, '14:00:00', 2),
+  (9, 4, 9, '20:00:00', 14),
+  (10, 4, 9, '23:00:00', 23),
+  (11, 5, 6, '09:30:00', 30),
+  (12, 5, 6, '12:30:00', 7),
+  (13, 5, 6, '14:30:00', 0),
+  (14, 5, 6, '15:20:00', 0),
+  (15, 6, 10, '10:00:00', 32),
+  (16, 6, 10, '13:30:00', 25),
+  (17, 6, 10, '17:00:00', 14),
+  (18, 7, 7, '12:00:00', 36),
+  (19, 8, 4, '15:00:00', 24),
+  (20, 9, 3, '17:00:00', 0);
+
 
 
 SELECT m.Movie_Name, s.Start_Time
@@ -64,6 +77,7 @@ FROM movie_info m
 JOIN showings s ON m.Movie_ID = s.Movie_ID
 WHERE s.Start_Time > '12:00:00' AND s.Available_Seats > 0
 ORDER BY s.Start_Time;
+
 
 
 SELECT m.Movie_Name
